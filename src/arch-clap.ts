5543ffc8a1ed3c043f37edd1b21c9b6050101022
Joel Chen <xchen@walmartlabs.com>
2020-03-03 09:11:42 -0800

    use object to prevent passing values like false

--- src/arch-clap.ts
+++ src/arch-clap.ts
@@ -59,7 +59,7 @@ function setupPath(): void {
  * @param obj object
  * @returns obj new object with property keys sorted
  */
-function sortObjKeys<T extends {}>(obj: T): T {
+function sortObjKeys<T extends object>(obj: T): T {
   return (Object.keys(obj) as (keyof T)[]).sort().reduce((newObj, key) => {
     newObj[key] = obj[key];
     return newObj;
